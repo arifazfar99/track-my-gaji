df266da34e6d64c09829ac2023cbbea4c6dd1b0e
arifazfar99 <arifazfar99@gmail.com>
2025-06-06 12:22:42 +0800

    edit footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-white text-black p-6 shadow">
       <div className="max-w-6xl mx-auto">
-        <p className="text-sm font-semibold">Made by Arif Azfar</p>
+        <p className="text-sm font-semibold">Made by Arif Azfar. All rights reserved. </p>
       </div>
     </footer>
   );
